44854cd5da98de9d436c30cc790acf1fc914bc24
SashaStvol <138790716+SashaStvol@users.noreply.github.com>
2025-11-07 16:40:43 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,3 @@
  */
 
 export { Color } from './Color.js';
-
-// Convenience named exports
-export { 
-    hexToNumber, 
-    numberToHex, 
-    rgbToNumber, 
-    numberToRgb,
-    rgbaToNumber,
-    numberToRgba,
-    isHex,
-    hsvToRgb,
-    rgbToHsv,
-    generatePaletteHSV,
-    generatePaletteGolden
-} from './Color.js';
